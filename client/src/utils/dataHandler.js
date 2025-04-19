d1c61d8c3870ed70e0120d0f6193324d46f9c759
vik782 <vincent.k.782@gmail.com>
2025-04-19 17:36:22 +1000

    Attempt using deploy URL

--- client/src/utils/dataHandler.js
+++ client/src/utils/dataHandler.js
@@ -11,13 +11,13 @@ const /** ?string */ token = cookies.get("TOKEN");
  * For dev-mode
  * ------------------------------------------------------------ */
 
-const URL = process.env.REACT_APP_LOCAL_URL;
+// const URL = process.env.REACT_APP_LOCAL_URL;
 
 /* ------------------------------------------------------------
  * For deployment-mode
  * ------------------------------------------------------------ */
-// const URL = process.env.REACT_APP_DEPLOY_URL;;
-             
+const URL = process.env.REACT_APP_DEPLOY_URL;
+
 /* ------------------------------------------------------------
  * At ${URL}/login
  *
@@ -30,15 +30,17 @@ const URL = process.env.REACT_APP_LOCAL_URL;
  * @param password the password provided by the user in the
  *                 loginpage
  */
-export async function getLoginPromise (username, password) {
-  return setupPromise(/* configuration = */ {
-    method: "post",
-    url: `${URL}/login`,
-    data: {
-      username,
-      password,
-    },
-  });
+export async function getLoginPromise(username, password) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "post",
+      url: `${URL}/login`,
+      data: {
+        username,
+        password,
+      },
+    }
+  );
 }
 
 /* ------------------------------------------------------------
@@ -52,147 +54,168 @@ export async function getLoginPromise (username, password) {
  * @param password the password provided by the user in the
  *                 loginpage
  */
-export async function getSignUpPromise (username, password) {
-  return setupPromise(/* configuration = */ {
-    method: "post",
-    url: `${URL}/signup`,
-    data: {
-      username,
-      password,
-    },
-  });
+export async function getSignUpPromise(username, password) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "post",
+      url: `${URL}/signup`,
+      data: {
+        username,
+        password,
+      },
+    }
+  );
 }
 
 /* ------------------------------------------------------------
  * At ${URL}/dashboard
  * ------------------------------------------------------------ */
-export async function getInitDashboardPromise () {
-  return setupPromise(/* configuration = */ {
+export async function getInitDashboardPromise() {
+  return setupPromise(
+    /* configuration = */ {
       method: "get",
       url: `${URL}/data`,
       headers: {
         // authorized route with jwt token
-        Authorization: `Bearer ${token}`, 
+        Authorization: `Bearer ${token}`,
       },
-    });
+    }
+  );
 }
 
-export async function getPagePromise (pageNum) {
-  return setupPromise(/* configuration = */ {
+export async function getPagePromise(pageNum) {
+  return setupPromise(
+    /* configuration = */ {
       method: "get",
       url: `${URL}/data/${pageNum}`,
       headers: {
         // authorized route with jwt token
-        Authorization: `Bearer ${token}`, 
+        Authorization: `Bearer ${token}`,
       },
-    });
+    }
+  );
 }
 
-export async function getSearchPromise (searchText) {
-  return setupPromise(/* configuration = */ {
+export async function getSearchPromise(searchText) {
+  return setupPromise(
+    /* configuration = */ {
       method: "get",
       url: `${URL}/search-artefacts/${searchText}`,
       headers: {
         // authorized route with jwt token
         Authorization: `Bearer ${token}`,
       },
-    });
+    }
+  );
 }
 
-export async function getSearchCategoryPromise (searchText, pageNum) {
-  return setupPromise(/* configuration = */ {
+export async function getSearchCategoryPromise(searchText, pageNum) {
+  return setupPromise(
+    /* configuration = */ {
       method: "get",
       url: `${URL}/search-category/${searchText}/${pageNum}`,
       headers: {
         // authorized route with jwt token
         Authorization: `Bearer ${token}`,
       },
-    });
+    }
+  );
 }
 
-export async function getSearchAssociatedPromise (searchText, pageNum) {
-  return setupPromise(/* configuration = */ {
+export async function getSearchAssociatedPromise(searchText, pageNum) {
+  return setupPromise(
+    /* configuration = */ {
       method: "get",
       url: `${URL}/search-associated/${searchText}/${pageNum}`,
       headers: {
         // authorized route with jwt token
         Authorization: `Bearer ${token}`,
       },
-    });
+    }
+  );
 }
 
 /* ------------------------------------------------------------
  * At ${URL}/add-artefact
  * ------------------------------------------------------------ */
-export async function postArtefact (record) {
-  return setupPromise(/* configuration = */ {
-    method: "post",
-    url: `${URL}/add-artefact`,
-    data: {
-      record,
-    },
-    headers: {
-      // authorized route with jwt token
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export async function postArtefact(record) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "post",
+      url: `${URL}/add-artefact`,
+      data: {
+        record,
+      },
+      headers: {
+        // authorized route with jwt token
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 }
 
-export async function getCategoryPromise (requestURI) {
-  return setupPromise(/* configuration = */ {
-    method: "get",
-    headers: {
-      // authorized route with jwt token
-      Authorization: `Bearer ${token}`,
-    },
-    url: `${URL}/${requestURI}`,
-  });
+export async function getCategoryPromise(requestURI) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "get",
+      headers: {
+        // authorized route with jwt token
+        Authorization: `Bearer ${token}`,
+      },
+      url: `${URL}/${requestURI}`,
+    }
+  );
 }
 
 /* ------------------------------------------------------------
  * At ${URL}/full-view
  * ------------------------------------------------------------ */
-export async function getFullViewPromise (id) {
-  return setupPromise(/* configuration = */ {
-    method: "get",
-    url: `${URL}/get-artefact/${id}`,
-    headers: {
-      Authorization: `Bearer ${token}`, // authorized route with jwt token
-    },
-  });
+export async function getFullViewPromise(id) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "get",
+      url: `${URL}/get-artefact/${id}`,
+      headers: {
+        Authorization: `Bearer ${token}`, // authorized route with jwt token
+      },
+    }
+  );
 }
 
-export async function updateArtefact (id, record) {
-  return setupPromise(/* configuration = */ {
-    method: "patch",
-    url: `${URL}/edit-artefact/${id}`,
-    data: {
-      record,
-    },
-    headers: {
-      Authorization: `Bearer ${token}`, // authorized route with jwt token
-    },
-  });
+export async function updateArtefact(id, record) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "patch",
+      url: `${URL}/edit-artefact/${id}`,
+      data: {
+        record,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`, // authorized route with jwt token
+      },
+    }
+  );
 }
 
 /* ------------------------------------------------------------
  * Others
  * ------------------------------------------------------------ */
-export async function deleteArtefact (id) {
-  return setupPromise(/* configuration = */ {
-    method: "delete",
-    url: `${URL}/delete-artefact/${id}`,
-    headers: {
-      // authorized route with jwt token
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export async function deleteArtefact(id) {
+  return setupPromise(
+    /* configuration = */ {
+      method: "delete",
+      url: `${URL}/delete-artefact/${id}`,
+      headers: {
+        // authorized route with jwt token
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 }
 
 /* ------------------------------------------------------------
  * Helper function
  * ------------------------------------------------------------ */
-async function setupPromise (configuration) {
+async function setupPromise(configuration) {
   return await axios(configuration);
 }
-
